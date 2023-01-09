d8818e54c4cacf6ee7e12eb776972214d6806852
nahuel <kamaelnahuel@gmail.com>
2023-01-09 19:55:38 -0300

    cambios en la estitica del item

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -18,18 +18,18 @@ const ItemDetail = ({ id, nombre, medida, imgSrc, precio, color, descripcion, ca
     return (
         <div className='row'>
             <div className='col-6 mt-5'>
-                    <img src={imgSrc} height="400" />
+                <img src={imgSrc} height="400" />
             </div>
-                <div className='col-6 mt-5'>
-                    <h2>{nombre} {medida} </h2>
-                    <p><strong>Categoria: </strong> {category}</p>
-                    <section className='mt-2'> <strong>Descripción: </strong> {descripcion}  </section>
-                    <p>{color}</p>
-                    <h4 className='mt-3'><strong> Precio: </strong> ${precio}</h4>
-                    <br />
-                    <button className="btn btn-outline-primary" onClick={botonVolver}>Volver</button>
-                    <button className="btn btn-primary m-3">Añadir al carrito</button>
-                </div>
+            <div className='col-6 mt-5'>
+                <h2>{nombre} {medida} </h2>
+                <h5 className='mt-3'><strong>Categoria: </strong> {category}</h5>
+                <section className='mt-4'> <strong>Descripción: </strong> {descripcion}  </section>
+                <p>{color}</p>
+                <h4 className='mt-4'><strong> Precio: </strong> ${precio}</h4>
+                <br />
+                <button className="btn btn-outline-primary" onClick={botonVolver}>Volver</button>
+                <button className="btn btn-primary m-3">Añadir al carrito</button>
+            </div>
         </div>
 
     )
